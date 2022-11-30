05e306815dcc3cfd26939f6624fbcc4268d730ac
sunandi <sunandi@ukur.com>
2022-11-30 14:32:48 +0700

    first commit

---
+++ src/app/home/juz/juz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juz',
+  templateUrl: './juz.component.html',
+  styleUrls: ['./juz.component.css']
+})
+export class JuzComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/home/surah/detail-surah/detail-surah.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail-surah',
+  templateUrl: './detail-surah.component.html',
+  styleUrls: ['./detail-surah.component.css']
+})
+export class DetailSurahComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
